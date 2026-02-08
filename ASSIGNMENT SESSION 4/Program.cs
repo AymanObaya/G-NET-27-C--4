28c1c2d2040a93ffa8b452871311ed73a4a0ef6d
AymanObaya <ayman.obaya2001@gmail.com>
2026-02-08 15:25:17 +0200

    Question 1

--- ASSIGNMENT SESSION 4/Program.cs
+++ ASSIGNMENT SESSION 4/Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region QUESTION 01
+
+            //(A)
+            //string productList = "";
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    productList += "PROD-" + i + ",";
+            //}
+
+            /*
+            immuImmutable type meaning its contents cannot be changed after creation. 
+            every time change or operation to string must create new object
+            that make more Memory allocations and more time
+            */
+
+            //(b)
+
+            //StringBuilder sb = new StringBuilder();
+
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    sb.Append("PROD-" + i + ",");
+            //}
+
+
+
+
+
+            //(c)
+            //var Watch01 = System.Diagnostics.Stopwatch.StartNew();
+            //string productList = "";
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    productList += "PROD-" + i + ",";
+            //}
+            //Watch01.Stop();
+
+            //Console.WriteLine($"string Time    :{Watch01.ElapsedMilliseconds}");
+
+
+
+            //var Watch02 = System.Diagnostics.Stopwatch.StartNew();
+            //StringBuilder sb = new StringBuilder();
+
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    sb.Append("PROD-" + i + ",");
+            //}
+            //Watch02.Stop();
+
+            //Console.WriteLine($"string Time    :{Watch02.ElapsedMilliseconds}"); 
+            #endregion
+
         }
     }
 }
